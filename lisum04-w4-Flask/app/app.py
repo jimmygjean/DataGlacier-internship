0550f4607b19f5cef38937276c1ef0530c2b4447
jimmygjean <jimmyg.jean@gmail.com>
2021-10-30 19:52:06 +0800

    update

--- lisum04-w4-Flask/app/app.py
+++ lisum04-w4-Flask/app/app.py
@@ -15,7 +15,11 @@ model = VGG16()
 app = Flask(__name__, template_folder='templates')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
 def predict():
     """The user is provided with an interface where
     he/she can upload an image. After submission,
@@ -23,37 +27,34 @@ def predict():
     of what the image could be. The result is then sent to
     the user.
     """
-    if request.method == 'GET':
-        out_message = None
-    elif request.method == 'POST':
-        # get file
-        file = request.files['imagefile']
-        filename = secure_filename(file.filename)
+    # get file
+    file = request.files['imagefile']
+    filename = secure_filename(file.filename)
 
-        if file is None or file.filename == "":
-            out_message = "No file"
-        elif not allowed_file(file.filename):
-            out_message = "Format not currently supported"
-        else:
-            # save image in database
-            image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename).replace("\\","/")
-            file.save(image_path)
+    if file is None or file.filename == "":
+        out_message = "No file"
+    elif not allowed_file(file.filename):
+        out_message = "Format not currently supported"
+    else:
+        # save image in database
+        image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename).replace("\\","/")
+        file.save(image_path)
 
-            # load & preprocess image
-            image = load_img(image_path, target_size=(224, 224))
-            image = img_to_array(image)
-            image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-            image = preprocess_input(image)
-            try:
-                # predict
-                y_pred = model.predict(image)
-                label = decode_predictions(y_pred)
-                label = label[0][0]
-                out_message = "Your image is a %s (%.2f%%)" %(label[1], label[2]*100)
-            except:
-                out_message = "An error occured during prediction"
+        # load & preprocess image
+        image = load_img(image_path, target_size=(224, 224))
+        image = img_to_array(image)
+        image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+        image = preprocess_input(image)
+        try:
+            # predict
+            y_pred = model.predict(image)
+            label = decode_predictions(y_pred)
+            label = label[0][0]
+            out_message = "Your image is a %s (%.2f%%)" %(label[1], label[2]*100)
+        except:
+            out_message = "An error occured during prediction"
 
     return render_template('index.html', out_message=out_message)
 
 if __name__ == '__main__':
-    app.run(port=3000, debug=True)
+    app.run(debug=True)
